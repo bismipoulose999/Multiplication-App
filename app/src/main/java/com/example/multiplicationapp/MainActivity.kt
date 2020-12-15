df428b1c7d1c4783c8c12040598c7a7f0e8e9593
bismipoulose999 <bismipoulose999@gmail.com>
2020-12-15 13:19:46 +0530

    multiplication app created

---
+++ app/src/main/java/com/example/multiplicationapp/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.multiplicationapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun mulevent(view: View) {
+        var getnum1=findViewById<EditText>(R.id.num1)
+        var getnum2=findViewById<EditText>(R.id.num2)
+        var result=getnum1.text.toString().toInt()*getnum2.text.toString().toInt()
+
+        Toast.makeText(this,result.toString(),Toast.LENGTH_LONG).show()
+    }
+}
